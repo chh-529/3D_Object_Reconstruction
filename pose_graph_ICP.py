ef323d3c6922775cd700816b2fd4ef14272f5499
謝承憲 <b12902145@ntu.edu.tw>
2026-04-01 00:55:45 +0800

    Replace the input data with TUM dataset(desk)

--- pose_graph_ICP.py
+++ pose_graph_ICP.py
@@ -47,79 +47,126 @@ def full_registration(pcds, max_correspondence_distance_coarse, max_corresponden
     pose_graph = o3d.pipelines.registration.PoseGraph()
     odometry = np.identity(4)
     pose_graph.nodes.append(o3d.pipelines.registration.PoseGraphNode(odometry))
-    n_pcds = len(pcds) # 16
-    for source_id in range(n_pcds):
-        for target_id in range(source_id + 1, n_pcds):
-            print('source id:', source_id)
-            print('target id:', target_id)
+    n_pcds = len(pcds)
+    
+    print("開始建立 Pose Graph (純里程計模式)...")
+    
+    # 🚨 關鍵修改：移除雙重迴圈，只讓 source_id 跟 source_id + 1 (相鄰幀) 做 ICP
+    for source_id in range(n_pcds - 1):
+        target_id = source_id + 1
+        print(f'正在對齊: Frame {source_id} -> Frame {target_id}')
 
-            init_trans = np.identity(4)
-            transformation_icp, information_icp = pairwise_registration(pcds_down[source_id], pcds_down[target_id],
-                                                                        init_trans)
-            print("Build o3d.pipelines.registration.PoseGraph")
-            if target_id == source_id + 1:  # odometry case
+        init_trans = np.identity(4)
+        transformation_icp, information_icp = pairwise_registration(
+            pcds[source_id], pcds[target_id], init_trans)
+            
+        # 累積位姿 (Odometry accumulation)
+        odometry = np.dot(transformation_icp, odometry)
 
-                odometry = np.dot((transformation_icp), odometry)
-
-                pose_graph.nodes.append(
-                    o3d.pipelines.registration.PoseGraphNode(
-                        np.linalg.inv(odometry)))
-                pose_graph.edges.append(
-                    o3d.pipelines.registration.PoseGraphEdge(source_id,
-                                                             target_id,
-                                                             transformation_icp,
-                                                             information_icp,
-                                                             uncertain=False))
-            else:  # loop closure case -> connect any non-neighboring nodes
-                pose_graph.edges.append(
-                    o3d.pipelines.registration.PoseGraphEdge(source_id,
-                                                             target_id,
-                                                             transformation_icp,
-                                                             information_icp,
-                                                             uncertain=True))
-    # Loop closure
-    init_trans = np.identity(4)
-    transformation_icp, information_icp = pairwise_registration(pcds_down[n_pcds-1], pcds_down[0],
-                                                                init_trans)
-    pose_graph.edges.append(
-        o3d.pipelines.registration.PoseGraphEdge(n_pcds-1,
-                                                 0,
-                                                 transformation_icp,
-                                                 information_icp,
-                                                 uncertain=False))
+        # 加入 Node (節點：相機當下的絕對位置)
+        pose_graph.nodes.append(
+            o3d.pipelines.registration.PoseGraphNode(
+                np.linalg.inv(odometry)))
+                
+        # 加入 Edge (邊：相機移動的相對位置)
+        pose_graph.edges.append(
+            o3d.pipelines.registration.PoseGraphEdge(
+                source_id, target_id, transformation_icp,
+                information_icp, uncertain=False))
+                
     return pose_graph
+
+# def full_registration(pcds, max_correspondence_distance_coarse, max_correspondence_distance_fine):
+#     pose_graph = o3d.pipelines.registration.PoseGraph()
+#     odometry = np.identity(4)
+#     pose_graph.nodes.append(o3d.pipelines.registration.PoseGraphNode(odometry))
+#     n_pcds = len(pcds) # 16
+#     for source_id in range(n_pcds):
+#         for target_id in range(source_id + 1, n_pcds):
+#             print('source id:', source_id)
+#             print('target id:', target_id)
+
+#             init_trans = np.identity(4)
+#             transformation_icp, information_icp = pairwise_registration(pcds_down[source_id], pcds_down[target_id],
+#                                                                         init_trans)
+#             print("Build o3d.pipelines.registration.PoseGraph")
+#             if target_id == source_id + 1:  # odometry case
+
+#                 odometry = np.dot((transformation_icp), odometry)
+
+#                 pose_graph.nodes.append(
+#                     o3d.pipelines.registration.PoseGraphNode(
+#                         np.linalg.inv(odometry)))
+#                 pose_graph.edges.append(
+#                     o3d.pipelines.registration.PoseGraphEdge(source_id,
+#                                                              target_id,
+#                                                              transformation_icp,
+#                                                              information_icp,
+#                                                              uncertain=False))
+#             else:  # loop closure case -> connect any non-neighboring nodes
+#                 pose_graph.edges.append(
+#                     o3d.pipelines.registration.PoseGraphEdge(source_id,
+#                                                              target_id,
+#                                                              transformation_icp,
+#                                                              information_icp,
+#                                                              uncertain=True))
+#     # Loop closure
+#     # init_trans = np.identity(4)
+#     # transformation_icp, information_icp = pairwise_registration(pcds_down[n_pcds-1], pcds_down[0],
+#     #                                                             init_trans)
+#     # pose_graph.edges.append(
+#     #     o3d.pipelines.registration.PoseGraphEdge(n_pcds-1,
+#     #                                              0,
+#     #                                              transformation_icp,
+#     #                                              information_icp,
+#     #                                              uncertain=False))
+#     return pose_graph
 
 if __name__ == "__main__":
 
-    object = "spyderman2"
+    # object = "spyderman2"
 
-    if object == "castard":
-        depth_path = ['./train/castard/depth/align_test_depth%d.png' % i for i in range(1, 21)]
-        rgb_path = ['./train/castard/rgb/align_test%d.png' % i for i in range(1, 21)]
-        pcds_paths = ['./pcd_o3d/castard/box%d.pcd' % i for i in range(1, 19)]
-    elif object == "new_box":
-        depth_path = ['./train/new_box2/depth/align_test_depth%d.png' % i for i in range(1, 19)]
-        rgb_path = ['./train/new_box2/rgb/align_test%d.png' % i for i in range(1, 19)]
-        pcds_paths = ['./pcd_o3d/new_box2/box1%d.pcd' % i for i in range(1, 19)]
-    elif object == "spyderman":
-        depth_path = ['./train/spyderman/depth/align_test_depth%d.png' % i for i in range(1, 17)]
-        rgb_path = ['./train/spyderman/rgb/align_test%d.png' % i for i in range(1, 17)]
-        pcds_paths = ['./pcd_o3d/spyderman/spyderman%d.pcd' % i for i in range(1, 17)]
-    elif object == "spyderman2":
-        depth_path = ['./train/spyderman/depth/align_test_depth%d.png' % i for i in range(1, 23)]
-        rgb_path = ['./train/spyderman/rgb/align_test%d.png' % i for i in range(1, 23)]
-        pcds_paths = ['./pcd_o3d/spyderman2/spyderman2%d.pcd' % i for i in range(1, 23)]
+    # if object == "castard":
+    #     depth_path = ['./train/castard/depth/align_test_depth%d.png' % i for i in range(1, 21)]
+    #     rgb_path = ['./train/castard/rgb/align_test%d.png' % i for i in range(1, 21)]
+    #     pcds_paths = ['./pcd_o3d/castard/box%d.pcd' % i for i in range(1, 19)]
+    # elif object == "new_box":
+    #     depth_path = ['./train/new_box2/depth/align_test_depth%d.png' % i for i in range(1, 19)]
+    #     rgb_path = ['./train/new_box2/rgb/align_test%d.png' % i for i in range(1, 19)]
+    #     pcds_paths = ['./pcd_o3d/new_box2/box1%d.pcd' % i for i in range(1, 19)]
+    # elif object == "spyderman":
+    #     depth_path = ['./train/spyderman/depth/align_test_depth%d.png' % i for i in range(1, 17)]
+    #     rgb_path = ['./train/spyderman/rgb/align_test%d.png' % i for i in range(1, 17)]
+    #     pcds_paths = ['./pcd_o3d/spyderman/spyderman%d.pcd' % i for i in range(1, 17)]
+    # elif object == "spyderman2":
+    #     depth_path = ['./train/spyderman/depth/align_test_depth%d.png' % i for i in range(1, 23)]
+    #     rgb_path = ['./train/spyderman/rgb/align_test%d.png' % i for i in range(1, 23)]
+    #     pcds_paths = ['./pcd_o3d/spyderman2/spyderman2%d.pcd' % i for i in range(1, 23)]
 
+    # 1. 改用 glob 自動讀取 pcd_o3d/ 裡面所有的 .pcd 檔案
+    # 注意：我們需要確保檔案是照順序排列的 (cloud_bin_0.pcd, cloud_bin_1.pcd ...)
+    import os
+    from glob import glob
+    
+    pcd_files = glob('./pcd_o3d/cloud_bin_*.pcd')
+    # 確保照數字順序排序，否則 Pose Graph 的相鄰關係會全錯！
+    pcd_files.sort(key=lambda x: int(os.path.basename(x).split('_')[2].split('.')[0]))
+    
+    pcds_paths = pcd_files
+    print(f"共找到 {len(pcds_paths)} 個點雲檔案準備進行配準。")
 
     # Define voxel size to Downsample
-    voxel_size = 0.001
+    # voxel_size = 0.001
+    voxel_size = 0.02
     origin_pcds = load_orginal_point_clouds(voxel_size, pcds_paths)
     pcds_down = load_point_clouds(voxel_size, pcds_paths)
-    o3d.visualization.draw_geometries(pcds_down)
+    # o3d.visualization.draw_geometries(pcds_down)
 
     print("Full registration ...")
-    max_correspondence_distance_coarse = voxel_size * 15
-    max_correspondence_distance_fine = voxel_size * 1
+    # max_correspondence_distance_coarse = voxel_size * 15
+    # max_correspondence_distance_fine = voxel_size * 1
+    max_correspondence_distance_coarse = voxel_size * 10
+    max_correspondence_distance_fine = voxel_size * 2
     with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Debug) as cm:
         pose_graph = full_registration(pcds_down,
                                        max_correspondence_distance_coarse,
@@ -141,10 +188,15 @@ if __name__ == "__main__":
 
     print("Transform points and display")
     accumulated_pcd = o3d.geometry.PointCloud()
-    for point_id in range(len(pcds_down)):
-        print(pose_graph.nodes[point_id].pose)
-        accumulated_pcd += pcds_down[point_id].transform(pose_graph.nodes[point_id].pose)
-    o3d.visualization.draw_geometries([accumulated_pcd])
+    # for point_id in range(len(pcds_down)):
+    #     print(pose_graph.nodes[point_id].pose)
+    #     accumulated_pcd += pcds_down[point_id].transform(pose_graph.nodes[point_id].pose)
+    for point_id in range(len(origin_pcds)):
+        # 複製一份原始點雲，避免改到原本的資料
+        pcd_temp = origin_pcds[point_id].transform(pose_graph.nodes[point_id].pose)
+        accumulated_pcd += pcd_temp
+
+    # o3d.visualization.draw_geometries([accumulated_pcd])
     # o3d.io.write_point_cloud('accumulated_%s.pcd'%object, accumulated_pcd)
 
     # y = np.asarray(accumulated_pcd.points)[:, 1]
@@ -158,18 +210,24 @@ if __name__ == "__main__":
     # o3d.visualization.draw_geometries([interest_pcd])
 
     # Render
-    vis = o3d.visualization.Visualizer()
-    vis.create_window('3DReconstructed')
+    # vis = o3d.visualization.Visualizer()
+    # vis.create_window('3DReconstructed')
 
-    for p in pcds_down:
-        vis.add_geometry(p)
+    # for p in pcds_down:
+    #     vis.add_geometry(p)
 
-    axis = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.1, origin=[0, 0, 0])
-    vis.add_geometry(axis)
+    # axis = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.1, origin=[0, 0, 0])
+    # vis.add_geometry(axis)
 
-    opt = vis.get_render_option()
-    opt.background_color = np.asarray([1, 1, 1])
-    opt.point_size = 1.5
+    # opt = vis.get_render_option()
+    # opt.background_color = np.asarray([1, 1, 1])
+    # opt.point_size = 1.5
 
-    vis.run()
-    vis.destroy_window()
+    # vis.run()
+    # vis.destroy_window()
+
+    # 假設最終合併好的點雲變數叫做 pcd_combined
+    # 將點雲存成 .ply 格式 (這是在 3D 領域最通用且支援顏色的格式)
+    print("正在儲存重建結果...")
+    o3d.io.write_point_cloud("reconstructed_result.ply", accumulated_pcd)
+    print("儲存成功！檔案名稱為 reconstructed_result.ply")
--- preprocess_pcd.py
+++ preprocess_pcd.py
@@ -1,7 +1,9 @@
+import os
 import cv2
 import open3d as o3d
 import numpy as np
 import matplotlib.pyplot as plt
+from dataloader import load_tum_pointclouds
 
 def rgbd_to_pcd(count):
 
@@ -86,6 +88,46 @@ def rgbd_to_pcd(count):
 
     o3d.io.write_point_cloud('./pcd_o3d/spyderman2/spyderman2%d.pcd'%count, cl)
 
-if __name__ == '__main__':
-    for i in range(1, 33):
-        rgbd_to_pcd(i)
+# if __name__ == '__main__':
+#     for i in range(1, 33):
+#         rgbd_to_pcd(i)
+
+if __name__ == "__main__":
+    DATASET_DIR = "dataset/tum_fr1_desk"
+    ASSOC_FILE = "dataset/tum_fr1_desk/associated.txt"
+    OUT_DIR = "pcd_o3d"
+    
+    os.makedirs(OUT_DIR, exist_ok=True)
+    
+    # 1. 透過 DataLoader 載入所有點雲
+    print("正在載入點雲資料...")
+    pcds = load_tum_pointclouds(DATASET_DIR, ASSOC_FILE)
+    
+    # ⚠️ 重要技巧：降採樣幀數 (Frame Subsampling)
+    # TUM 資料集動輒 700~800 張照片，全跑 ICP 和 Pose Graph 會算到天荒地老。
+    # 為了快速驗證你的架構，我們先「每 10 張取 1 張」來做重建。
+    pcds_subset = pcds[::10] 
+    
+    print(f"取樣後共剩下 {len(pcds_subset)} 張點雲準備進行前處理。")
+    
+    # 2. 迴圈處理並存檔
+    for i, pcd in enumerate(pcds_subset):
+        print(f"Processing point cloud {i}...")
+        
+        # --- 這裡可以保留原作者的過濾邏輯 ---
+        # 1. 體素降採樣 (Voxel Downsampling) 以加速運算
+        pcd_down = pcd.voxel_down_sample(voxel_size=0.02)
+        
+        # 2. 移除離群雜訊 (Statistical Outlier Removal)
+        cl, ind = pcd_down.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)
+        pcd_clean = pcd_down.select_by_index(ind)
+        
+        # ⚠️ 注意：我這裡先移除了原作者的「移除平面」與「DBSCAN 聚類」
+        # 因為 TUM fr1/desk 是一個「場景 (辦公桌)」，如果你跑單一物件提取，
+        # 桌子和背景會不見，只剩下桌上的一個小杯子，會導致後面特徵對齊失敗。
+        
+        # 3. 將處理好的點雲依照原 Repo 的命名規則存檔
+        filename = os.path.join(OUT_DIR, f"cloud_bin_{i}.pcd")
+        o3d.io.write_point_cloud(filename, pcd_clean)
+        
+    print(f"前處理完成！所有點雲已存入 {OUT_DIR}/ 資料夾。")
